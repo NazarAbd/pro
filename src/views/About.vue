78669cd55ae99040e6b84f4127d700b96f7c93d4
nazarAbd <nazarsd63@gmail.com>
2022-01-10 19:09:04 -0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <h1>This is <span> {{name}}</span></h1>
+    <h2>His age is <span>{{age}}</span> and he is opsessed with<span>{{opsessed}}</span></h2>
+  </div>
+</template>
+<script>
+export default {
+  data(){
+    return{
+      name:'Nazar',
+      age:23,
+      opsessed:'Front-End Devlopment',
+    }
+  }
+}
+</script>
+<style scoped>
+span{
+  color:#42b983
+}
+</style>
